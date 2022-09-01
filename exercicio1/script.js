c7d884d9e3ae34237267b358afbc3e0209cbc56a
Nana-shi <leof.pereira2@gmail.com>
2022-09-01 19:38:30 -0300

    Atividades aula 01/09 a010-arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+/* EXercicio 1
+
+No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+- O segundo array deve possuir apenas **strings**;
+- O terceiro array deve possuir **números**, **strings** e **booleanos**.
+Imprima cada um dos arrays acima, utilizando `console.log()`*/
+
+const arrayNum = [89, 69, 35, 74]
+const arrayStrings = ["Pão", "Batata", "Biscoito", "Bolacha"]
+const arrayMistu = [69, "Tubarão", true]
+
+console.log("Array Numeros:", arrayNum)
+console.log("Array Strings:", arrayStrings)
+console.log("Array Misturado:", arrayMistu)
+
+/*Fim da primeira atividade*/
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,26 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+/*Exercicio 2
+
+Com os arrays criados, agora vamos observar os arrays. Faça o que se pede abaixo, utilizando `console.log()`:
+
+- Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+- Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+- Imprima uma informação booleana verdadeira sobre a **inclusão** de um item do primeiro array, e uma informação booleana falsa sobre a **inclusão** de um item ao terceiro array. Isto é, um `includes()` verdadeiro, e outro falso.
+*/
+
+const arrayNum = [89, 69, 35, 74]
+const arrayStrings = ["Pão", "Batata", "Biscoito", "Bolacha"]
+const arrayMistu = [69, "Tubarão", true]
+
+console.log("Array Numeros:", arrayNum)
+console.log("Array Strings:", arrayStrings)
+console.log("Array Misturado:", arrayMistu)
+
+console.log("Array Numeros tamanho:", arrayNum.length)
+console.log("Array Strings tamanho:", arrayStrings.length)
+console.log("Array Misturado tamanho:", arrayMistu.length)
+
+console.log("Conteudos 1,2 e 3 dos respectivos Arrays:", arrayNum[0], arrayStrings[1], arrayMistu[2])
+
+console.log("Array Numeros contem \"69\":", arrayNum.includes(69), "\nArray Misturado contem \"Feijoada\":", arrayMistu.includes("Feijoada"))
+
+/*Fim da segunda atividade*/
